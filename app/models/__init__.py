af3176dcc33494b89914ea72f5f6f7369794f70d
Szczypiorrr <wintheer09@gmail.com>
2026-06-24 23:53:59 +0200

    feat: register all models in database initialization

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+from .user import User
+from .expense import Expense
+from .category import Category
+from .account import Account
+from .income import Income
+from .budget import Budget
+from .goal import Goal
