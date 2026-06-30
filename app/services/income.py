2b88774509a2a3c1c38c1ccc537faf39418ad731
Szczypiorrr <wintheer09@gmail.com>
2026-06-30 17:30:27 +0200

    feat: add income service layer for CRUD operations

---
+++ app/services/income.py
@@ -0,0 +1,74 @@
+from models.income import Income
+from schemas.income import IncomeCreate, IncomeUpdate
+from sqlalchemy.orm import Session
+from services.account import get_account_by_id
+from datetime import datetime
+
+class IncomeNotFound(Exception):
+    pass
+
+class InvalidAmount(Exception):
+    pass
+
+def get_incomes(db: Session, account_id: int | None = None, start_date: datetime | None = None, end_date: datetime | None = None, limit: int = 10, offset: int = 0):
+    query = db.query(Income)
+
+    if account_id is not None:
+        query = query.where(Income.account_id == account_id)
+
+    if start_date is not None:
+        query = query.where(Income.created_at >= start_date)
+
+    if end_date is not None:
+        query = query.where(Income.created_at <= end_date)
+
+    return query.limit(limit).offset(offset).all()
+
+def get_income_by_id(income_id: int, db: Session):
+    income = db.query(Income).where(Income.id == income_id).first()
+
+    if not income:
+        raise IncomeNotFound()
+    
+    return income
+
+def create_income(income: IncomeCreate, db: Session):
+    get_account_by_id(account_id=income.account_id, db=db)
+
+    income_db = Income(
+        amount=income.amount,
+        source=income.source,
+        created_at=datetime.now(),
+        account_id=income.account_id
+    )
+
+    db.add(income_db)
+    db.commit()
+    db.refresh(income_db)
+
+    return income_db
+
+def delete_income(income_id: int, db: Session):
+    income = get_income_by_id(income_id=income_id, db=db)
+
+    db.delete(income)
+    db.commit()
+
+    return
+
+def update_income(income_id: int, income_update: IncomeUpdate, db: Session):
+    income = get_income_by_id(income_id=income_id, db=db)
+
+    if income_update.amount is not None:
+        if income_update.amount <= 0:
+            raise InvalidAmount()
+        
+        income.amount = income_update.amount
+
+    if income_update.source is not None:
+        income.source = income_update.source
+
+    db.commit()
+    db.refresh(income)
+
+    return income
