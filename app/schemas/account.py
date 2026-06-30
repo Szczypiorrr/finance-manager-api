ee1dbac76074e393df37b2e07b73691c2bba76f5
Szczypiorrr <wintheer09@gmail.com>
2026-06-30 11:52:48 +0200

    feat: add deposit, withdraw, transfer and update schemas

--- app/schemas/account.py
+++ app/schemas/account.py
@@ -2,18 +2,38 @@ from pydantic import BaseModel
 
 class AccountBase(BaseModel):
     name: str
-    balance: float
 
 class AccountCreate(AccountBase):
     user_id: int
 
-class AccountUpdate(BaseModel):
-    name: str | None = None
-    balance: float | None = None
+class AccountUpdate(AccountBase):
+    pass
+
+class AccountDeposit(BaseModel):
+    amount: float
+
+class AccountWithdraw(BaseModel):
+    amount: float
+
+class AccountTransfer(BaseModel):
+    sender_id: int
+    receiver_id: int
+    amount: float
 
 class AccountResponse(AccountBase):
+    balance: float
     id: int
     user_id: int
+
+    class Config:
+        from_attributes = True
+
+class AccountTransferResponse(BaseModel):
+    amount: float
+    sender_id: int
+    sender_balance: float
+    receiver_id: int
+    receiver_balance: float
 
     class Config:
         from_attributes = True
