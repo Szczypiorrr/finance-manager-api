8f9cf5bd268f8fddb8cf6ae7017467b66201f931
Szczypiorrr <wintheer09@gmail.com>
2026-06-30 21:18:38 +0200

    refactor: update budget request and response schemas

--- app/schemas/budget.py
+++ app/schemas/budget.py
@@ -1,20 +1,18 @@
 from pydantic import BaseModel
-from datetime import date
 
 class BudgetBase(BaseModel):
     limit_amount: float
-    month: date
 
 class BudgetCreate(BudgetBase):
     user_id: int
     category_id: int
 
-class BudgetUpdate(BaseModel):
-    limit_amount: float | None = None
-    month: date | None = None
-    category_id: int | None = None
+class BudgetUpdate(BudgetBase):
+    pass
 
-class BudgetResponse(BaseModel):
+class BudgetResponse(BudgetBase):
+    month: int
+    year: int
     id: int
     user_id: int
     category_id: int
