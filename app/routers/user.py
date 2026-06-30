c9550c733c88f82c315651972ded5dbac1cef1ad
Szczypiorrr <wintheer09@gmail.com>
2026-06-30 12:03:15 +0200

    refactor: simplify routes and rename username field

--- app/routers/user.py
+++ app/routers/user.py
@@ -4,13 +4,13 @@ from sqlalchemy.orm import Session
 from core.database import get_db
 import services.user as user_service
 
-router = APIRouter(tags=["Users"])
+router = APIRouter(tags=["Users"], prefix="/users")
 
-@router.get("/users/", response_model=list[UserResponse])
+@router.get("/", response_model=list[UserResponse])
 def read_users(db: Session = Depends(get_db)):
     return user_service.get_users(db=db)
 
-@router.get("/users/{user_id}", response_model=UserResponse)
+@router.get("/{user_id}", response_model=UserResponse)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     try:
         return user_service.get_user_by_id(user_id=user_id, db=db)
@@ -18,14 +18,14 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
 
-@router.post("/users/", response_model=UserResponse)
+@router.post("/", response_model=UserResponse)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     try:
         return user_service.create_user(username=user.username, db=db)
     except user_service.UserAlreadyExists:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="User with this username already exists")
 
-@router.delete("/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_user(user_id: int, db: Session = Depends(get_db)):
     try:
         return user_service.delete_user(user_id=user_id, db=db)
@@ -33,10 +33,10 @@ def delete_user(user_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
         
 
-@router.put("/users/{user_id}", response_model=UserResponse)
+@router.put("/{user_id}", response_model=UserResponse)
 def update_user(user_id: int, user: UserUpdate, db: Session = Depends(get_db)):
     try:
-        return user_service.update_user(user_id=user_id, new_username=user.new_username, db=db)
+        return user_service.update_user(user_id=user_id, username=user.username, db=db)
     except user_service.UserAlreadyExists:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="User with this username already exists")
     except user_service.UserNotFound:
