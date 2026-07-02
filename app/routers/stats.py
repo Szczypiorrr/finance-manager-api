0d6dc1dcf1abedda9c01b253856b9eb828a239f0
Szczypiorrr <wintheer09@gmail.com>
2026-07-02 14:13:33 +0200

    feat: add stats router with CRUD endpoints

---
+++ app/routers/stats.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from schemas.stats import MonthlyStatsResponse , ByCategoryStatsResponse, MonthlyTrendResponse, TopExpensesResponse
+from sqlalchemy.orm import Session
+from core.database import get_db
+import services.stats as stats_service
+import services.user as user_service
+
+router = APIRouter(tags=["Stats"], prefix="/stats")
+
+@router.get("/monthly", response_model=MonthlyStatsResponse)
+def read_monthly_stats(month: int | None = None, year: int | None = None, db: Session = Depends(get_db)):
+    return stats_service.get_monthly_stats(month=month, year=year, db=db)
+
+@router.get("/by-category", response_model=list[ByCategoryStatsResponse])
+def read_stats_by_category(db: Session = Depends(get_db)):
+    return stats_service.get_stats_expenses_by_category(db=db)
+
+@router.get("/balance")
+def read_stats_balance(user_id: int | None = None, account_id: int | None = None, db: Session = Depends(get_db)):
+    try:
+        return stats_service.get_stats_balance(user_id=user_id, account_id=account_id, db=db)
+    except user_service.UserNotFound:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    except stats_service.AccountNotFound:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Account not found")
+    
+@router.get("/top-expenses", response_model=list[TopExpensesResponse])
+def read_top_expenses(limit: int = 10, offset: int = 0, user_id: int | None = None, account_id: int | None = None, db: Session = Depends(get_db)):
+    try:
+        return stats_service.get_top_expenses(limit=limit, offset=offset, user_id=user_id, account_id=account_id, db=db)
+    except user_service.UserNotFound:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    except stats_service.AccountNotFound:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Account not found")
+    
+@router.get("/monthly-trend", response_model=list[MonthlyTrendResponse])
+def read_monthly_trend(user_id: int | None = None, account_id: int | None = None, db: Session = Depends(get_db)):
+    try:
+        return stats_service.get_monthly_trend(user_id=user_id, account_id=account_id, db=db)
+    except user_service.UserNotFound:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    except stats_service.AccountNotFound:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Account not found")
+    
